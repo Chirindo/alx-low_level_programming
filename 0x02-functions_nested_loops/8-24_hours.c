ec07f46991fbbb59abe1bc8014e0f26fa0100f8c
Chirindo <jessicahchimweri@gmail.com>
2023-02-22 16:50:51 -0800

    There are only 3 colors, 10 digits, and 7 notes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints minutes
+ *
+ * Return: no return
+ */
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a<= 2; a++)
+		_putchar(a + 48);
+}
