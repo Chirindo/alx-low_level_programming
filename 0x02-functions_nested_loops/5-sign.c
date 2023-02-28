178290c205b36e59c30c281915474f05324e007f
Chirindo <jessicahchimweri@gmail.com>
2023-02-28 15:20:11 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,29 +2,27 @@
 
 /**
  * main print_sign - greater, equal or less than zero
- * @n: The input is an integer
- *  Return: 1 is greater than 0. 0 is zero
- *  -1 is less than zero.
+ * @n: The integer to be checked
+ * Return: 1 and prints + if n is greater than zero
+ * -1 and prints - if n is less than zero
  */
-
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
-	{
-		_putchar(45);
-		return (-1);
-	}
-	else
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
 	}
-	_putchar('\n');
+	else if (n < 0)
+	{
+		_putchar('_');
+	}
+	return (-1);
 }
 
 
