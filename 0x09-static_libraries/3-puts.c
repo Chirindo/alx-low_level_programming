e26375037b9b3141e3238d59dafa8f1a04815704
Chirindo <jessicahchimweri@gmail.com>
2023-03-10 15:38:36 -0800

    task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - prints a string followed by a new line
+ * @str: string function
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		putchar(*str);
+	}
+	putchar('\n');
+}
